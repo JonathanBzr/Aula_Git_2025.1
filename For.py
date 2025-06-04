4e30b346f51efc47dc8749ec1e9141b0207777ce
Jonathan Freire <freire.jonathan@escolar.ifrn.edu.br>
2025-06-04 20:12:02 -0300

    Trabalho não finalizado

---
+++ For.py
@@ -0,0 +1,38 @@
+#Questão 1
+"""
+for valor in range (2, 13, 2):
+    print(valor, end= ", ")
+"""
+
+#Questão 2
+"""
+Numero = int(input("Insira um número positivo: "))
+
+Valor = 1
+
+for Valor in range (1, Numero + 1):
+    print(Valor, end= ", ")
+"""
+
+#Questão 3
+"""
+Inicio = int(input("Insira o valor inicial do intervalo: "))
+Fim = int(input("Insira o valor final do intervalo: "))
+
+base = 1
+for base in range (Inicio, Fim+1):
+    print(base, end=", ")
+"""
+
+#Questão 4
+
+Valor1 = int(input("Insira um número inteiro: "))
+Valor2 = int(input("Insira outro número inteiro: "))
+
+base = 1
+
+for base in range (Valor1+1, Valor2):
+    print(base, end=", ")
+
+print("\n")
+print("Fim do programa!")
---
+++ while.py
@@ -0,0 +1,31 @@
+#Questão 01
+
+"""
+numero = 1
+
+while numero <= 100:
+    print(numero)
+    numero +=1
+
+print("Programa encerrado!")
+"""
+
+"""
+#Questão 2
+
+Somatorio = 0
+Valor = 1
+
+while (Valor != 0):
+    print(f"A soma é {Somatorio}")
+    Valor = int(input("Digite um valor inteiro ou 0 para sair: "))
+    Somatorio = Somatorio + Valor
+
+print("\n")
+print("Término do programa")
+"""
+
+#questão 3
+
+Senha = input("Digite uma senha: ")
+print(f"{Senha}")
